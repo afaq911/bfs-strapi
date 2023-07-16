a662d286de2f603098dce82f79c72d23c1e10c2f
kreativeAfaq <kreative.afaqhaider@gmail.com>
2023-07-16 17:55:03 +0500

    CHECK EMAIL

--- src/api/contact-number/controllers/contact-number.js
+++ src/api/contact-number/controllers/contact-number.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * contact-number controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::contact-number.contact-number');
+module.exports = createCoreController("api::contact-number.contact-number");
