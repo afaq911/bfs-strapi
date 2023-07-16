ac6d914ec9842d31b2752190560cda663af477e1
kreativeAfaq <kreative.afaqhaider@gmail.com>
2023-07-17 01:22:39 +0500

    NEW FORGOT PASS SFTP

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   email: {
-    config: {
-      provider: "strapi-provider-email-smtp",
-      providerOptions: {
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: true,
-        username: env("USER_EMAIL_COMPANY"),
-        password: env("USER_PASSWORD_COMPANY"),
-        rejectUnauthorized: true,
-        requireTLS: true,
-        connectionTimeout: 1,
+    provider: "nodemailer",
+    providerOptions: {
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: true,
+      auth: {
+        user: env("USER_EMAIL_COMPANY"),
+        pass: env("USER_PASSWORD_COMPANY"),
       },
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
     },
     settings: {
       defaultFrom: env("EMAIL_RECIPT_RECIEVER"),
@@ -19,5 +19,27 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   email: {
+//     config: {
+//       provider: "strapi-provider-email-smtp",
+//       providerOptions: {
+//         host: "smtp.gmail.com",
+//         port: 587,
+//         secure: true,
+//         username: env("USER_EMAIL_COMPANY"),
+//         password: env("USER_PASSWORD_COMPANY"),
+//         rejectUnauthorized: true,
+//         requireTLS: true,
+//         connectionTimeout: 1,
+//       },
+//     },
+//     settings: {
+//       defaultFrom: env("EMAIL_RECIPT_RECIEVER"),
+//       defaultReplyTo: env("EMAIL_RECIPT_SENDER"),
+//     },
+//   },
+// });
 
 // port: 465,
